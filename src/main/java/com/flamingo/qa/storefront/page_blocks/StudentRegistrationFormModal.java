1e1f0b96200cad667bc8d5c81a1c72a142b9336d
Oleksandr Sarana <>
2025-11-30 15:21:15 -0500

    add student form modal elements and actions

--- src/main/java/com/flamingo/qa/storefront/page_blocks/StudentRegistrationFormModal.java
+++ src/main/java/com/flamingo/qa/storefront/page_blocks/StudentRegistrationFormModal.java
@@ -2,74 +2,88 @@ package com.flamingo.qa.storefront.page_blocks;
 
 import com.flamingo.qa.helpers.web.page.UIComponent;
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.flamingo.qa.storefront.page_block_elements.StudentRegistrationFormModalElements.*;
 
 @Component
 public class StudentRegistrationFormModal extends UIComponent {
 
     @Step("Get firstname")
     public String getFirstName() {
-        return getText();
+        return getText(NAME_FIELD_XPATH).split(" ")[0];
     }
 
     @Step("Get lastname")
     public String getLastName() {
-        return getText();
+        return getText(NAME_FIELD_XPATH).split(" ")[1];
     }
 
     @Step("Get email")
     public String getEmailField() {
-        return getText();
+        return getText(EMAIL_FIELD_XPATH);
     }
 
     @Step("Get gender")
     public String getGender() {
-        return getText();
+        return getText(GENDER_FIELD_XPATH);
     }
 
     @Step("Get phone number")
     public String getPhoneNumber() {
-        return getText();
+        return getText(MOBILE_FIELD_XPATH);
     }
 
     @Step("Get date of birth")
     public String getDateOfBirth() {
-        return getText();
+        return getText(DATE_OF_BIRTH_FIELD_XPATH);
     }
 
-    @Step("Gte subjects")
+    @Step("Get subjects")
     public List<String> getSubjects() {
-        return getTexts();
+        return Arrays.stream(getText(SUBJECTS_FIELD_XPATH).split(",\\s*"))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
-    @Step("Select hobbies")
+    @Step("Get hobbies")
     public List<String> getHobbies() {
-        return getTexts();
+        return Arrays.stream(getText(HOBBIES_FIELD_XPATH).split(",\\s*"))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    @Step("Get picture name")
+    public String getPictureName() {
+        return getText(PICTURE_FIELD_XPATH);
     }
 
     @Step("Get address")
     public String getAddress() {
-        return getText();
+        return getText(ADDRESS_FIELD_XPATH);
     }
 
     @Step("Get city")
     public String getCity() {
-        return getText();
+        return getText(STATE_AND_CITY_FIELD_XPATH).split(" ")[1];
     }
 
     @Step("Get state")
     public String getState() {
-        return getText();
+        return getText(STATE_AND_CITY_FIELD_XPATH).split(" ")[0];
     }
 
     @Step("Close Student submission pop-up")
     public void closeModal() {
-
+        click(By.id(CLOSE_BUTTON_ID));
     }
 
     public boolean isOpened() {
-        return ;
+        return isDisplayed(FORM_POP_UP_BLOCK_XPATH);
     }
 }
